266e4f0e532e1a550429c1b9f6d57b5d1b554846
alemale2001 <79916469+alemale2001@users.noreply.github.com>
2021-04-04 13:18:40 +0200

    Add files via upload
    Los nuevos y creo que completamente funcionales modulos Ususarios, aun tengo que mirar un par de cosas pero esta casi 100% completo, usad estos de ahora en adelante.
    P.D: No se poner lo que necesita Pepe, a ver si David o Abel me echan una mano en esto para que Pepe pueda testear su módulo.

---
+++ _Usuarios.h
@@ -0,0 +1,14 @@
+#ifndef _Usuarios
+#define _Usuarios
+#include <stdio.h>
+#include <string.h>
+#include "carga.h"
+#include "Cronista.h"
+#include "administrador.h"
+
+void users();
+void acceso();
+void registro();
+
+
+#endif
