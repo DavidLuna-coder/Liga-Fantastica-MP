88d88aafb621693322b6a3a40718df2f68e99e07
PAELLAROSA <67125496+PAELLAROSA@users.noreply.github.com>
2021-04-08 19:21:19 +0200

    Add files via upload

---
+++ participante.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef participante
+#define participante
+
+void participantes();
+void Cplantilla();
+void addJug(int *i);
+void remoJug(int *i);
+
+#endif
