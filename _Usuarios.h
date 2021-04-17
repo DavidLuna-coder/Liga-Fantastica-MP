9a6d520733b8b1f8160adeda34a0442478be0a9e
alemale2001 <79916469+alemale2001@users.noreply.github.com>
2021-04-17 18:00:22 +0200

    Add files via upload
    Ahora sin las cosas de testeo de por medio, creo

---
+++ _Usuarios.h
@@ -0,0 +1,15 @@
+#ifndef _Usuarios
+#define _Usuarios
+#include <stdio.h>
+#include <string.h>
+#include "carga.h"
+#include "Cronista.h"
+#include "administrador.h"
+#include "participante.h"
+int usuarioActual;
+void users();
+void acceso();
+void registro();
+
+
+#endif
