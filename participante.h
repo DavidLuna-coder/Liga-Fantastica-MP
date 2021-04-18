9a4f56952be3f0e1fbb9e9caf212a9d16d279966
alemale2001 <79916469+alemale2001@users.noreply.github.com>
2021-04-18 19:53:06 +0200

    Add files via upload

---
+++ participante.h
@@ -0,0 +1,14 @@
+
+#include "carga.h"
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef participante
+#define participante
+
+void participantes();
+void Cplantilla();
+void configu();
+void addJug();
+void remoJug();
+void listarPlantillas();
+#endif
