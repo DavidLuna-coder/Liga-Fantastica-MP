44919e79d934add0df618e9a41a0216a5bf5f5c3
PAELLAROSA <67125496+PAELLAROSA@users.noreply.github.com>
2021-04-20 17:19:58 +0200

    cronista.c(documentado)

--- Cronista.c
+++ Cronista.c
@@ -4,13 +4,13 @@
 #include "Cronista.h"
 //void cronista(jugador *);
 
-
+    //cabecera: void cronista();
+    //precondicion: tener cargados los usuarios y los jugadores
+    //postcondicion: permite ejecutar todas las funciones de el cronista
 void cronista(){
-int elec,res;
-
-
-
-do{
+int elec;   //elec: variable para selecionar una opcion en el switch
+    
+do{      //menu de selecion de las funciones del cronista
  printf("1-mostrar lista de jugadores y sus valoraciones\n");
 printf("2-valorar un jugador\n");
 printf("3-Salir de la sesion\n");
@@ -25,11 +25,11 @@ switch(elec){
     break;
 
     case 3:
-    printf("saliendo de la sesión...\n");
+    printf("saliendo de la sesion...\n");
     break;
 
     default:
-    printf("introduce un numero valido [1,2 ó 3]: \n");
+    printf("introduce un numero valido [1,2 o 3]: \n");
     break;
 }   
     
@@ -37,6 +37,9 @@ switch(elec){
 system("pause");
 }
 
+    //cabecera: void lista();
+    //precondicion: debe tener cargado los jugadores 
+    //postcondicion: muestra una lista de todos los jugadores
 void lista(){
 int i;
 for(i=0;i<numeroJugadores;i++){
@@ -44,14 +47,20 @@ for(i=0;i<numeroJugadores;i++){
 }
 }
 
+
+    //cabecera: void valorar();
+    //precondicion: debe tener cargado los jugadores 
+    //postcondicion: otorga una valoracion a un jugador elegido por su ID
 void valorar (){
-    int id,i,val,find=0;
+    int id,i,val,find=0;    //id: id temporar que sirve para compararla con la id real que buscamos
+                            //val: valor que regula el valor maximo y minimo que un jugador puede tomar [0-10]
+                            //find: variable que marca si el jugador ha sido encontrado
     printf("introduce la id del jugador a valorar: ");
     scanf("%i",&id);
 
     for(i=0;i<numeroJugadores || find==0;i++){
         if(jugadores[i].id==id){
-            printf("/n introduzca una valoracion entre 0-10 para %s: ",jugadores[i].nombre);
+            printf("\n introduzca una valoracion entre 0-10 para %s: ",jugadores[i].nombre);
             scanf("%i",&val);
             while(val<0 || val>10){
                 printf("introduce un valor entre 0-10\n");
@@ -62,4 +71,5 @@ void valorar (){
         }
     }
 }
+
 
