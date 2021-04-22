b3089ac26ce06e0be72abc633b29ae9b789821d1
4bysss <74374776+4bysss@users.noreply.github.com>
2021-04-22 08:55:13 +0200

    Update administrador.h

--- administrador.h
+++ administrador.h
@@ -4,16 +4,4 @@
 #include <string.h>
 #include "carga.h"
 void menuAdmin();
-void listarEquipos();
-void listarUsuarios();
-void modificarUsuarios();//falta case 5
-void anadirUsuario();
-void modificarPresupuestos();
-void eliminarUsuario();
-void anadirEquipo();
-void limitePlantillas();
-void borrarEquipo();
-void jugadoresPlantilla();
-void disponibilidadJugadores();
-void disponibilidadJugadores2();
 #endif
